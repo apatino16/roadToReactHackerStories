baffa71a0d5cc349da357e3dce3d2c0c239773af
Anny Patino <a.a.patino16@gmail.com>
2023-03-21 00:55:48 -0700

    Refactored functions into arrowe functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const list = [
+const list = () => [
   {
     title: "React",
     url: "https://reactjs.org/",
@@ -19,43 +19,36 @@ const list = [
   },
 ];
 
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text" />
-    </div>
-  );
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search"> Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}> {item.title}</a>
-            </span>
-            <span> {item.author}</span>
-            <span> {item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}> {item.title}</a>
+        </span>
+        <span> {item.author}</span>
+        <span> {item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+    ;
+  </ul>
+);
 
-function App() {
-  return (
-    <div>
-      <h1> My Hacker Stories </h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1> My Hacker Stories </h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
 
 export default App;
