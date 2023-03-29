034fa87450ad8ee013d1d50cebb0d6c4deb88126
Anny Patino <a.a.patino16@gmail.com>
2023-03-29 11:33:47 -0700

    Added an onChange event handler

--- src/App.js
+++ src/App.js
@@ -19,12 +19,17 @@ const list = () => [
   },
 ];
 
-const Search = () => (
-  <div>
-    <label htmlFor="search"> Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+  };
+  return (
+    <div>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
 
 const List = () => (
   <ul>
