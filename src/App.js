55d0f5c4220bd0d988023e97ce53b5ef4d76fb9b
Anny Patino <a.a.patino16@gmail.com>
2023-04-03 10:28:17 -0700

    useState change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import * as React from "react";
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
   const handleChange = (event) => {
-    console.log(event);
+    setSearchTerm(event.target.value);
   };
   return (
     <div>
       <label htmlFor="search"> Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 };
