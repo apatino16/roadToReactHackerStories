f080f93c87d67cf28332febc63268e57e4176dcd
Anny Patino <a.a.patino16@gmail.com>
2023-05-07 18:59:32 -0700

    the stories, now returned as a stateful list from Reacts useState Hook, are still filtered into searchedStories and displayed in the List component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import * as React from "react";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -13,26 +32,9 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+
+  const [stories, setStories] = React.useState(initialStories);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
